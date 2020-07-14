411665a1d50ba2aa28b24369cf45f8ed60fce867
Mathias <mathias.grandin@gmail.com>
2020-07-14 15:09:55 +0200

    Aggiunti commenti e una ol in html

--- js/script.js
+++ js/script.js
@@ -3,18 +3,23 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-
+// numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
+// numeri divisibili per 3 e 5
   if (i % 3 == 0 && i % 5 == 0) {
-  console.log("FizzBuzz");
-
+    document.getElementById('list').innerHTML += "<li>" + "FizzBuzz" + "</li>";
+    console.log("FizzBuzz");
+// numeri divisibili per 3
   } else if (i % 3 == 0) {
-  console.log("Fizz");
-
+    document.getElementById('list').innerHTML += "<li>" + "Fizz" + "</li>";
+    console.log("Fizz");
+// numeri divisibili per 5
   } else if (i % 5 == 0) {
-  console.log("Buzz");
-
+    document.getElementById('list').innerHTML += "<li>" + "Buzz" + "</li>";
+    console.log("Buzz");
+// altri numeri
   }  else {
-  console.log(i);
+    document.getElementById('list').innerHTML += "<li>" + i + "</li>";
+    console.log(i);
   }
 }
